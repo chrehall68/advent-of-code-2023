77f51586108ac2cbcdaae8432c1c091d2d698566
chrehall68 <chrehall68@gmail.com>
2023-12-07 22:26:14 -0800

    add template for easier future coding

---
+++ template/solution.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+
+
+def part1(lines: List[str]) -> int:
+    return 0
+
+
+def part2(lines: List[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    lines = open("input.txt", "r").read().strip().split("\n")
+    print(part1(lines))
+    print(part2(lines))
